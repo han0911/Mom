f95659bcf5ba5271c138721b9c9546b8b0f24427
han0911 <a01099249559@gmail.com>
2026-01-18 17:45:17 +0900

    첫 커및

---
+++ pages/api/[...nextauth].js
@@ -0,0 +1,39 @@
+import { connectDB } from "@/util/database";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID, 
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  session: {
+    strategy: 'jwt',
+    maxAge: 30 * 24 * 60 * 60 
+  },
+
+  callbacks: {
+    jwt: async ({ token, user }) => {
+      if (user) {
+        token.user = {
+          name: user.name,
+          email: user.email,
+          image: user.image 
+        };
+      }
+      return token;
+    },
+    session: async ({ session, token }) => {
+      session.user = token.user;  
+      return session;
+    },
+  },
+
+  adapter: MongoDBAdapter(connectDB),
+  secret: process.env.NEXTAUTH_SECRET,
+};
+
+export default NextAuth(authOptions);
